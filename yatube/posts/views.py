a51ede107ab6173cf6370c97258b31df442ea8a1
Олег Петренко <oleg.pallada@gmail.com>
2023-03-06 00:36:09 +0400

    вывод информации из базы данных

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,20 @@
 from django.http import HttpResponse
 
 from django.shortcuts import render
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post
 
 
 def index(request):
-    title = 'Это главная страница проекта Yatube'
-    context = {'title': title}
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию
+    # (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'posts': posts,
+    }
     return render(request, 'posts/index.html', context)
 
 
